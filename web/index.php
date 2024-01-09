af096f16ffcf8567c860d4cb45cdce6c3b56b968
OswaldoGSDAW <147555767+OswaldoGSDAW@users.noreply.github.com>
2024-01-09 23:34:05 +0100

    Creación de carpetas para MVC

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aplicacion</title>
+</head>
+<body>
+    <h1>Aplicación MVC</h1>
+</body>
+</html>
